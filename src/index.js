2441463d9ab8412e0215bb78860d713b92430715
Linananana07 <113578711+Linananana07@users.noreply.github.com>
2023-01-20 12:08:02 +0600

    fix: brackets code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+  let string = str;
+  for (let i in bracketsConfig) {
+      arr[i] = bracketsConfig[i][0] + bracketsConfig[i][1];
+  }
+  for (let i = 0; i <= str.length; i++) {
+      for (let j in bracketsConfig) {
+          string = string.split(`${arr[j]}`).join('');
+      }
+  }
+  if (string.length == 0) {
+      return (true);
+  } else if (string.length != 0) {
+      return (false);
+  }
 }
